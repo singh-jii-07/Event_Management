9b6fe318fb6802e2572bdf376b10995b8798e7b9
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-01 20:21:38 -0500

    Event Create Controller done

---
+++ Backend/App/Controller/Event.js
@@ -0,0 +1,37 @@
+import Event from "../Model/Event.js";
+
+const create = async (req, res) => {
+    try{
+
+    
+  const { title, description, date, time, location } = req.body;
+
+  if (!title || !description || !date || !time || !location) {
+    return res.status(400).json({
+      message: "All Filed All Require",
+    });
+  }
+
+  const newCreate = await Event( {
+    title,
+    description,
+    date,
+    time,
+    location,
+  });
+  newCreate.save();
+
+  res.status(200).json({
+    message: "Event Create Done",
+    newCreate,
+  });
+}
+catch (error) {
+    console.error("EVENT CREATE ERROR ", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export {create}
