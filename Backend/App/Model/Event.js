a72bb889a396bc56cfaa669d9ed3609fee6c767a
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-01 20:06:46 -0500

    event model done

---
+++ Backend/App/Model/Event.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const eventSchema = new mongoose.Schema({
+   
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: String,
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Event =mongoose.model ("event", eventSchema)
+
+export default Event
