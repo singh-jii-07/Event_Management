ceb5c8954a93e9fece6fabe4a0dd39d164e18aec
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-02 20:58:08 -0500

    getSingleEvent route done

--- Backend/App/Routes/Event.js
+++ Backend/App/Routes/Event.js
@@ -1,12 +1,13 @@
 import Express from 'express'
 import Auth from '../Middleware/Auth.js'
 import isAdmin from '../Middleware/isAdmin.js'
-import { create, deleteEvent, editEvent, getevent } from '../Controller/Event.js'
+import { create, deleteEvent, editEvent, getevent,getSingleEvent } from '../Controller/Event.js'
 
 const eventRoutes=Express.Router()
 
 eventRoutes.post("/create",Auth,isAdmin,create)
 eventRoutes.get("/get",getevent)
+eventRoutes.get("/single/:id",getSingleEvent)
 eventRoutes.delete("/delete/:id",Auth,isAdmin,deleteEvent)
 eventRoutes.put("/edit/:id",Auth,isAdmin,editEvent)
 export default eventRoutes
