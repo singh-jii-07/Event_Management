0283dc7200ccc3a501817d9cbc7e3eb0e9ba1ecd
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-02 20:42:45 -0500

    delete Routes done

--- Backend/App/Controller/Event.js
+++ Backend/App/Controller/Event.js
@@ -48,22 +48,27 @@ const getevent = async (req, res) => {
 
 const deleteEvent = async (req, res) => {
   try {
-    const event = Event.findById(req.params.id);
+    const event = await Event.findById(req.params.id);
+
     if (!event) {
       return res.status(404).json({ message: "Event not found" });
     }
-    await event.deleteOne();
 
-    res.status(200).json({
-      message: "Event Delete Done",
-      event,
+    await Event.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({
+      message: "Event deleted successfully",
     });
-  } catch (error) {
-    console.error("EVENT DELETE ERROR ", error);
+
+  } catch (err) {
+    console.error("EVENT DELETE ERROR ", err);
+
     return res.status(500).json({
-      message: "Internal Server Error",
+      message: "Failed to delete event",
+      error: err.message, 
     });
   }
 };
+
 
 export { create, getevent, deleteEvent };
--- Backend/App/Routes/Event.js
+++ Backend/App/Routes/Event.js
@@ -1,11 +1,11 @@
 import Express from 'express'
 import Auth from '../Middleware/Auth.js'
 import isAdmin from '../Middleware/isAdmin.js'
-import { create, getevent } from '../Controller/Event.js'
+import { create, deleteEvent, getevent } from '../Controller/Event.js'
 
 const eventRoutes=Express.Router()
 
 eventRoutes.post("/create",Auth,isAdmin,create)
 eventRoutes.get("/get",getevent)
-
+eventRoutes.delete("/delete/:id",Auth,isAdmin,deleteEvent)
 export default eventRoutes
