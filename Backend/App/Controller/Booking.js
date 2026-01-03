0e27b26eb6803f807dacff7a9c3d57360e51c7db
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-02 21:25:27 -0500

    createBookinng controller done

---
+++ Backend/App/Controller/Booking.js
@@ -0,0 +1,34 @@
+import Booking from '../Model/Booking.js'
+import Event from '../Model/Event.js'
+const createBookinng =async (req,res)=>{
+    try{
+        const {eventid}= req.body
+        const  userid= req.user.id
+        if (!eventId) {
+      return res.status(400).json({ message: "Event ID required" });
+    }
+
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+  const booking = await Booking({
+    user:userid,
+    event:eventid
+  })
+  booking.save()
+  res.status(201).json({
+      message: "Event booked successfully",
+      booking,
+    });
+    }
+    catch(err){
+        res.status(500).json({
+            message:"internal server Error",
+            error: err.message,
+        })
+    
+    }
+}
+
+export {createBookinng}
