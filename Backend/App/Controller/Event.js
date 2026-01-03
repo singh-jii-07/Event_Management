829dc51d191cc698a4131fe182ab6a6267c29cff
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-02 20:56:19 -0500

    getSingleEvent controller done

--- Backend/App/Controller/Event.js
+++ Backend/App/Controller/Event.js
@@ -95,7 +95,20 @@ const editEvent =async (req, res) =>{
     res.status(500).json({ message: err.message });
   }
 };
+const getSingleEvent = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    res.status(200).json(event);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 
 
-export { create, getevent, deleteEvent, editEvent };
+export { create, getevent, deleteEvent, editEvent,getSingleEvent };
