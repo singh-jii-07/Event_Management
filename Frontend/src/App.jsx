52c7b8c86a376ef757658f9aa88233e0319b9f1d
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-01-15 19:30:15 -0500

    routes done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,31 @@ import Allevent from "./Components/Allevent"
 import Login from "./Components/Login"
 import Profile from "./Components/Profile"
 import Signup from "./Components/Signup"
-
+import { RouterProvider,createBrowserRouter  } from "react-router-dom";
 
 function App() {
-  
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Allevent />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
 
   return (
     <>
-     {/* <Login/> */}
-     {/* <Signup/> */}
-     {/* <Profile/> */}
-     <Allevent/>
+     <RouterProvider router={router} />;
     </>
   )
 }
